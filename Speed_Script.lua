6c7361da6a6d1f693705a98c91f2bd13f5b06a77
eliasclaudiano32-jpg <eliasclaudiano32@gmail.com>
2026-06-23 22:28:04 -0300

    aaa

--- Speed_Script.lua
+++ Speed_Script.lua
@@ -1,13 +1,16 @@
--- Speed Script | Brookhaven
+-- Speed Script | Travado no 9
 -- Pressione Q para ativar/desativar
 -- Compatível com Xeno Executor
 
 local Players = game:GetService("Players")
 local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
 
 local player = Players.LocalPlayer
 local speedAtivado = false
 local VELOCIDADE = 9
+
+local loop
 
 local function Setup()
     local character = player.Character or player.CharacterAdded:Wait()
@@ -20,19 +23,33 @@ local function Setup()
             speedAtivado = not speedAtivado
 
             if speedAtivado then
-                humanoid.WalkSpeed = VELOCIDADE
                 print("[Speed] ✅ Speed ativado! WalkSpeed: " .. VELOCIDADE)
+                -- Loop que força o speed travado
+                loop = RunService.Heartbeat:Connect(function()
+                    if humanoid and humanoid.Parent then
+                        humanoid.WalkSpeed = VELOCIDADE
+                    end
+                end)
             else
-                humanoid.WalkSpeed = 16
                 print("[Speed] ❌ Speed desativado!")
+                if loop then
+                    loop:Disconnect()
+                    loop = nil
+                end
+                humanoid.WalkSpeed = 16
             end
         end
     end)
 
     player.CharacterAdded:Connect(function(newCharacter)
         local newHumanoid = newCharacter:WaitForChild("Humanoid")
+        if loop then loop:Disconnect() loop = nil end
         if speedAtivado then
-            newHumanoid.WalkSpeed = VELOCIDADE
+            loop = RunService.Heartbeat:Connect(function()
+                if newHumanoid and newHumanoid.Parent then
+                    newHumanoid.WalkSpeed = VELOCIDADE
+                end
+            end)
         end
     end)
 end
